92f88a6b583dbf6aaec18c3b9478e5378a1e7d42
CoderXDY <444875571@qq.com>
2017-02-18 14:16:02 +0800

    setjs

---
+++ setjs.js
@@ -0,0 +1,24 @@
+/*
+*/
+var Set = (function(){
+
+	var has = function(){
+	};
+
+	return{
+		//并集
+		union : function(x, y){},
+		//交集
+		intersection: function(x, y){},
+		//减集
+		differ : function(x, y){},
+		//增加元素
+		add : function(set, x){},
+		//去掉元素
+		remove : function(set, y){},
+		//是否元素
+		has : has
+
+	};
+
+})();
